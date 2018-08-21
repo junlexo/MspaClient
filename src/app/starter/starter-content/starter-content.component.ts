626e4b62d039b0a2ead652b525e7c6f57754f44b
DESKTOP-L25S3AV\junle <thanh55th2@gmail.com>
2018-08-22 00:25:39 +0700

    update client

--- src/app/starter/starter-content/starter-content.component.ts
+++ src/app/starter/starter-content/starter-content.component.ts
@@ -8,12 +8,19 @@ declare var AdminLTE: any;
   styleUrls: ['./starter-content.component.css']
 })
 export class StarterContentComponent implements OnInit {
-
+	show_roomtype: boolean = false;
   constructor() { }
 
   ngOnInit() {
     // Update the AdminLTE layouts
     AdminLTE.init();
   }
-
+  showSelectRoomType(){
+  	this.show_roomtype = !this.show_roomtype;
+  }
+  selectRoomType(event){
+  	var target = event.target || event.srcElement || event.currentTarget;
+  	var _index = $(".df_overlay span.df_component.df_label").index(target);
+  	$(".hour-col.hour-roomtype").find(".btn-hour-content-wrap").html($(".df_overlay span.df_dropdown_option").eq(_index).html());
+  }
 }
